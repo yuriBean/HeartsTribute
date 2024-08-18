09650ee90d250ebe68324a58e0d286bcf584585b
yuriBean <145260306+yuriBean@users.noreply.github.com>
2024-08-18 20:46:42 +0500

    cors2

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,42 +1,36 @@
-// Import packages
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import compression from "compression";
+import helmet from "helmet";
 import router from "./services/idrive.services.js";
 
-// Configure environment variables
 dotenv.config();
-
-// Initialize app
 const app = express();
-
-// Middleware
+app.use(helmet());
 app.use(express.json({ limit: "20mb" }));
 app.use(compression());
 
 // Allowed origins for CORS
 const allowlist = ['https://app.heartstribute.com', 'https://www.app.heartstribute.com'];
 
-// CORS configuration
 const corsOptionsDelegate = (req, callback) => {
   const origin = req.header('Origin');
+  console.log('Origin:', origin);  
+
   const corsOptions = allowlist.includes(origin) ? { origin: true } : { origin: false };
   callback(null, corsOptions);
 };
 
-// Apply CORS with options delegate
 app.use(cors(corsOptionsDelegate));
+app.options('*', cors(corsOptionsDelegate));
 
-// API endpoints
 app.use("/api", router);
 
-// Fallback for unmatched routes
 app.use((req, res) => {
   res.status(404).json({
     result: "Not found",
   });
 });
 
-// Export app
 export default app;
